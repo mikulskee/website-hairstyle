ed37fa9cc4415e6531f021dcdc1b8f959135dc78
Mateusz Mikulski <mikulskee@gmail.com>
2019-06-21 21:03:17 +0200

    Updates

---
+++ precache-manifest.1eddc522316caae8e22529200ed84b9a.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3eac3d481d879e75f11e5546c603898",
+    "url": "/website-hairstyle/index.html"
+  },
+  {
+    "revision": "cee4375ef58cf35873b8",
+    "url": "/website-hairstyle/static/js/2.d21bccf8.chunk.js"
+  },
+  {
+    "revision": "ffe2de95be9ef7746d4e",
+    "url": "/website-hairstyle/static/js/main.11eb32d3.chunk.js"
+  },
+  {
+    "revision": "143d2c65843dcc6e13a9",
+    "url": "/website-hairstyle/static/js/runtime~main.235c998d.js"
+  },
+  {
+    "revision": "155e190faa27b564bedcd68c219e2fef",
+    "url": "/website-hairstyle/static/media/Marta1.155e190f.svg"
+  },
+  {
+    "revision": "a56f54c34c070cf61cfa457c8a291bd3",
+    "url": "/website-hairstyle/static/media/Marta2.a56f54c3.svg"
+  },
+  {
+    "revision": "41fda4ff99d11bdb87182af53b02ed27",
+    "url": "/website-hairstyle/static/media/blob.41fda4ff.svg"
+  },
+  {
+    "revision": "b0b9dd1f06de33df093a093830aa7837",
+    "url": "/website-hairstyle/static/media/blob2.b0b9dd1f.svg"
+  },
+  {
+    "revision": "6d7e189ded34f794bc0ebb4df75c79cd",
+    "url": "/website-hairstyle/static/media/blob3.6d7e189d.svg"
+  },
+  {
+    "revision": "327ac58c9b16551edde7a4a9d49c2af0",
+    "url": "/website-hairstyle/static/media/blob4.327ac58c.svg"
+  },
+  {
+    "revision": "f07597158bc2070c74d083c3d5f4ae88",
+    "url": "/website-hairstyle/static/media/certificate.f0759715.svg"
+  },
+  {
+    "revision": "38d60e8b2a7979f2467bdc1f25838b8a",
+    "url": "/website-hairstyle/static/media/certificate2.38d60e8b.svg"
+  },
+  {
+    "revision": "da52373afd73ede191c7ee6e6b64ab6b",
+    "url": "/website-hairstyle/static/media/girl-one.da52373a.svg"
+  },
+  {
+    "revision": "2667dc2df57df40676953435781ccb9a",
+    "url": "/website-hairstyle/static/media/girl-three.2667dc2d.svg"
+  },
+  {
+    "revision": "5e27e4074d2e9bdfd3227d7d87b0fc04",
+    "url": "/website-hairstyle/static/media/girl-two.5e27e407.svg"
+  },
+  {
+    "revision": "17acdfdea149f78f5f9ee1f1628bcf93",
+    "url": "/website-hairstyle/static/media/hero-image-mobile.17acdfde.png"
+  },
+  {
+    "revision": "2542e5cfa929a7bd267eeb00abd4b996",
+    "url": "/website-hairstyle/static/media/hero-image.2542e5cf.png"
+  },
+  {
+    "revision": "9625cf5e9b447964167da9278eb8d629",
+    "url": "/website-hairstyle/static/media/icon-facebook.9625cf5e.svg"
+  },
+  {
+    "revision": "351e3d87975e1eaa23f62d5c7e3c150c",
+    "url": "/website-hairstyle/static/media/icon-instagram2.351e3d87.svg"
+  },
+  {
+    "revision": "19ccb4190677dee2de394e3869598fe2",
+    "url": "/website-hairstyle/static/media/lines-pattern.19ccb419.svg"
+  },
+  {
+    "revision": "bf4166ea1f39a1c55e96cd8722b65dc7",
+    "url": "/website-hairstyle/static/media/logo-main-white.bf4166ea.svg"
+  },
+  {
+    "revision": "c6784122c1d2c9b88c3e8389ee368c72",
+    "url": "/website-hairstyle/static/media/logo-main.c6784122.svg"
+  },
+  {
+    "revision": "b0ef79e28fa6578de0777f6a8695d131",
+    "url": "/website-hairstyle/static/media/motto-girl.b0ef79e2.png"
+  },
+  {
+    "revision": "a4057763feb813a9c0ecf3ba2f76b6fc",
+    "url": "/website-hairstyle/static/media/video-hair.a4057763.mp4"
+  },
+  {
+    "revision": "24670d861a5fbcb5258258e574b775d1",
+    "url": "/website-hairstyle/static/media/video-hair2.24670d86.mp4"
+  }
+]);
